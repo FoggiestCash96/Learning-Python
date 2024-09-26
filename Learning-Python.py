3c50cea3cafc305d41ee62d2b09fd36aebda6d91
Garrett Clark <garrett405472@gmail.com>
2024-09-26 11:25:48 -0500

    What I've learned in python so far

---
+++ Learning-Python.py
@@ -0,0 +1,203 @@
+# # my_str = 'http://reddit.com/r/python'
+# # protocol = 'http://'
+# # print(my_str[len(protocol:)])
+
+# # print('balls')
+
+# # my_str = 'http://reddit.com/r/python'
+# # protocol = 'http://'
+# # print(my_str[len(protocol):])
+
+# # stride_len = int(input())
+# # my_string = 'tangerine'
+
+# # sliced_string = my_string[0:stride_len:2]
+
+# # print('Full string:', my_string)
+# # print('Sliced string:', sliced_string)
+
+# # string_of_letters = input()
+# # index = string_of_letters.find('i', 0, 4)
+# # print(f'At index: {index}')
+# # print(string_of_letters.replace('i', '(blank)'))
+
+
+# # string_of_letters = input()
+# # if string_of_letters 
+
+# # string_of_letters = input()
+# # index = string_of_letters.find('i')
+# # print(f'At index: {index}')
+# # print(string_of_letters.replace('i', '(blank)', (index - 2)))
+
+# replace_string = input()
+# tongue_twister = input()
+
+# x = tongue_twister.rfind(replace_string)
+
+# final = tongue_twister.replace(replace_string, '##', 2)
+
+
+# if tongue_twister == final:
+#     print('No matches')
+# else:
+#     print(f'Located at: {x}')
+#     print(f'{final}')
+
+#     text[::2]
+
+# print('---------------------------')
+
+# my_string = input()
+
+# x = my_string[:2:]
+# y = my_string[2::]
+
+# print('---------------------------')
+
+
+# if x.islower() and y.isupper():
+#     print('Valid')
+# else:
+#     print('Invalid')
+
+#     string_data = input()
+
+# x = string_data.strip()
+
+# if 'Color' in x:
+#     print(x.lower())
+# else:
+#     print(x.upper())
+
+# print('-------------------------------')
+
+# item_info = 'Mug 8 15'
+
+# item_tokens = item_info.split()
+# item = item_tokens[0]
+# quantity = item_tokens[1]
+# price = item_tokens[2]
+
+# print(f'{item} stock: {quantity}')
+# print(f'Price: {price}')
+
+# print('-------------------------------')
+
+# item_info = 'Headphones 11 19'
+
+# item_tokens = item_info.split()
+# item = item_tokens[0]
+# quantity = item_tokens[1]
+# price = item_tokens[2]
+
+# print(f'{item} stock: {quantity}')
+# print(f'Price: {price}')
+
+# print('----------------------------------')
+
+# path = '/Users/Jon'
+
+# separator = '+'
+# path_tokens = path.split('/')
+# print(separator.join(path_tokens))
+
+# print('--------------------')
+
+# all_colors = input()
+# new_choice = input()
+
+# x = all_colors.join()
+# color_list = x
+
+# print(color_list)
+
+# gas_milage = input()
+# gas_cost = input()
+
+# x = gas_milage * gas_cost
+
+# print(x)
+
+# x = 12
+# y = str(x)
+# print(y)
+
+# x = 1234
+# print(x)
+
+# full_name = input()
+
+# split_name = full_name.split()
+# x = len(split_name)
+# if x == 3:
+#     name0 = split_name[0]
+#     abv_name0 = name0[0]
+#     name1 = split_name[1]
+#     abv_name1 = name1[0]
+#     name2 = split_name[2]
+# else x == 2:
+#     name0 = split_name[0]
+#     abv_name0 = name0[0]
+#     name1 = split_name[1]
+#     abv_name1 = name1[0]
+
+# print(f'{name2}, {abv_name0}.{abv_name1}.')
+
+# print(x)
+
+# txt = "Hello, welcome to my world."
+
+# x = txt.find("e")
+
+# print(x)
+
+# text = "n hello"
+# print(text[2:])
+
+# input = input()
+# split_input = input.split()
+# letter = split_input[0]
+# word1 = split_input[1]
+# word2 = split_input[2]
+
+# number_of_letter_word1 = word1.count(letter)
+# number_of_letter_word2 = word2.count(letter)
+
+# if len(split_input) == 2 or 3:
+#     if number_of_letter_word1 == 1:
+#         print(f"{number_of_letter_word1} {letter}")
+#     else:
+#         print(f"{number_of_letter_word1} {letter}'s")
+# elif len(split_input) > 3:
+#     if number_of_letter_word1 == 1:
+#         print(f"{number_of_letter_word1 + number_of_letter_word2} {letter}")
+#     else:
+#         print(f"{number_of_letter_word1 + number_of_letter_word2} {letter}'s")
+# print(number_of_letter_word1)
+# print(number_of_letter_word2)
+
+# number_of_toppings = input()
+# crust_type = input()
+# sauce_type = input()
+# meat_type = input()
+
+# a = input()
+# b = input()
+# c = input()
+# if a < b and a < c:
+#     print(a)
+# elif b < a and b < c:
+#     print(b)
+# elif c < a and c < b:
+#     print(c)
+
+a = input()
+b = input()
+c = input()
+
+if c == 3:
+    print(c)
+
+
+
